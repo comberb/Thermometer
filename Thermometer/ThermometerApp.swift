5126c6bc38728bd8cb73f57d2f7fca4190506d2b
Ben Comber <bfgcomber@gmail.com>
2023-08-25 08:58:51 +0100

    Initial Commit

---
+++ Thermometer/ThermometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermometerApp.swift
+//  Thermometer
+//
+//  Created by Ben Comber on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThermometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
